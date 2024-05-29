46e02a51d3d1a4bab782f047084cae5720ab87fd
GeebeeFrost <omgbolahan1@gmail.com>
2024-05-29 03:29:43 +0100

    Test service methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'gee-trello';
+export class AppComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe({
+      next: (res) => console.log('res', res),
+      error: (e) => {
+        console.error('err', e);
+        this.authService.setCurrentUser(null);
+      },
+    });
+  }
 }
